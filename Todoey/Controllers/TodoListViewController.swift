fd848aac30f34bf912e4928cb31a63c156a73944
Ice <ice@macbook-pro-4.local>
2019-01-18 18:00:39 +0700

    add Contrast Colour and Gradient Colour

---
+++ Todoey/Controllers/TodoListViewController.swift
@@ -0,0 +1,156 @@
+
+import UIKit
+import RealmSwift
+import ChameleonFramework
+
+class TodoListViewController: SwipeTableViewController {
+ 
+    var todoItems: Results<Item>?
+    let realm = try! Realm()
+    
+    var selectedCategory : Category? {
+        didSet{
+            loadItems()
+            //print("SelectedCategory : \((selectedCategory!.name ?? ""))")
+            print("SelectedCategory : \((selectedCategory!.name ))")
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.separatorStyle = .none
+    }
+    
+    //MARK - Tableview Datasource Medthods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoItems?.count ?? 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
+        
+        if let item = todoItems?[indexPath.row]{
+            cell.textLabel?.text = item.title
+            
+            if let colour =  UIColor(hexString: selectedCategory!.colour)?.darken(byPercentage:CGFloat(indexPath.row) / CGFloat(todoItems!.count)){
+                cell.backgroundColor = colour
+                cell.textLabel?.textColor = ContrastColorOf(colour, returnFlat: true)
+            }
+            cell.accessoryType = item.done ? .checkmark : .none
+        
+        }else{
+            cell.textLabel?.text = "No Items Added"
+        }
+        
+        return cell
+    }
+
+    //MARK - TableView Delegate Medthods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if let item = todoItems?[indexPath.row]{
+            do{
+                try realm.write {
+                    item.done = !item.done
+                }
+            }catch{
+                print("Cant write \(error)")
+            }
+        }
+
+        tableView.reloadData()
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+    //MARK - Add New Items
+    
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        //var UserText : String = ""
+        var TextField = UITextField()
+        let alert = UIAlertController(title: "Add new ToDoey", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //What will action when user clicked
+            
+            if let currentCategory = self.selectedCategory{
+                do{
+                    try self.realm.write {
+                        let newItem = Item()
+                        newItem.title = TextField.text!
+                        newItem.dateCreated = Date()
+                        currentCategory.items.append(newItem)
+                    }
+                }catch{
+                    print("Cant write realm \(error)")
+                }
+            }
+            
+            self.tableView.reloadData()
+            
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Item"
+            TextField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    //MARK - Model Manipulation Methods
+    
+    
+    func loadItems(){
+        todoItems = selectedCategory?.items.sorted(byKeyPath: "title", ascending: true) //Get Data from title
+        //todoItems = realm.objects(Item.self)  //Get All Data In Item
+        self.tableView.reloadData()
+    }
+    
+    override func updateModel(at indexPath: IndexPath) {
+        
+        if let item = self.todoItems?[indexPath.row]{
+            do{
+                try self.realm.write {
+                    self.realm.delete(item)
+                }
+            }catch{
+                print("Error deleting Item, \(error)")
+            }
+        }
+        
+    }
+    
+    
+}
+
+//MARK: - Search Bar Methods
+
+extension TodoListViewController: UISearchBarDelegate{
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        todoItems = todoItems?.filter("title CONTAINS[cd] %@", searchBar.text!).sorted(byKeyPath: "dateCreated", ascending: true)
+        
+        tableView.reloadData()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            self.loadItems()
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            } //Hidekeyboard When click cancle search
+            
+        }
+    }
+    
+}
